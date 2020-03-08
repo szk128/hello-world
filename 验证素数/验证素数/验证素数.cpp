d3d106cf097c52119dbafb6d1aafcfe416fb613a
szk128 <60804987+szk128@users.noreply.github.com>
2020-03-08 18:18:01 +0800

    push

---
+++ 验证素数/验证素数/验证素数.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int test = 1;
+    int m = 0;
+    int r = 0;
+    for (int i = 0; i < 40; i++)
+    {
+        m = i * i + i + 41;
+        for (int j = 2; j < m; j++)
+        {
+            r = m % j;
+            if (r == 0 ) { cout << m << endl; test = 0; break; }
+        }
+    }
+    if (test == 1) cout << 'N' << endl;
+}
+
